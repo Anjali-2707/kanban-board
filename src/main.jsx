cb90ca45d1c4e65b3ca8c957c6f9bd2def199c16
Anjali-2707 <gurjaranjali2707@gmail.com>
2025-04-23 12:38:01 +0530

    frontend: routing and screen structure done

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Login from './components/login-page.jsx'
+import Board from './components/board.module.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: 'login',
+        element: <Login/>
+      },
+      {
+        path: '',
+        element: <Board/>
+      }]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
